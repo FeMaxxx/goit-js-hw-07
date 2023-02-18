648428ce1420bc2684190dc59f54e67e741671b9
FeMaxxx <119453050+FeMaxxx@users.noreply.github.com>
2023-02-18 16:35:47 +0200

    end

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,51 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeCard = ({ preview, original, description }) =>
+  `<div id="myButton" class="gallery__item">
+    <a id="myModal" class="gallery__link">
+      <img class="gallery__image" src="${preview}" alt="${description}" data-source="${original}">
+    </a>
+  </div>`;
+
+const cards = galleryItems.map(makeCard).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", cards);
+
+const giveOriginalImg = (e) => {
+  e.preventDefault();
+
+  if (e.target.nodeName !== "IMG") {
+    return;
+  }
+
+  showModal(e.target);
+};
+
+galleryEl.addEventListener("click", giveOriginalImg);
+
+const showModal = (imagelink) => {
+  const original = imagelink.dataset.source;
+  const instance = basicLightbox.create(
+    `
+    <div class="modal">
+      <a><img class="modal-image" src="${original}"></a>
+    </div>
+  `
+  );
+
+  document.addEventListener("keydown", (e) => {
+    closeModal(e, instance);
+  });
+
+  instance.show();
+};
+
+const closeModal = (e, instance) => {
+  if (e.key === "Escape") {
+    document.removeEventListener("keydown", closeModal);
+    instance.close();
+  }
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,26 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+
+const makeCard = ({ preview, original, description }) =>
+  `
+    <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}"/>
+    </a>
+  `;
+
+const cards = galleryItems.map(makeCard).join("");
+
+galleryEl.insertAdjacentHTML("beforeend", cards);
+
+let gallery = new SimpleLightbox(".gallery a", {
+  captions: true,
+  captionSelector: "img",
+  captionType: "attr",
+  captionsData: "alt",
+  captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+galleryEl.addEventListener("click", giveOriginalImg);
